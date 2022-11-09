c20387314cffae0d88694aa9a063ae516cfaa71b
Benjamin Edward Webb <benjaminedwardwebb@gmail.com>
2022-11-08 23:33:22 -0500

    home: organize packages into separate file

--- home/home.nix
+++ home/home.nix
@@ -28,6 +28,8 @@ in
   home.enableNixpkgsReleaseCheck = true;
 
   imports = [
+    ./packages.nix
+
     ./bash
     ./dircolors
     ./direnv
@@ -47,6 +49,7 @@ in
     ./tmux
     ./wezterm
     ./xdg
+
     (
       with builtins;
       let
@@ -58,76 +61,4 @@ in
       else { }
     )
   ];
-
-  home.packages = [
-    # desktop environment
-    pkgs.i3
-    pkgs.i3status-rust
-    pkgs.dmenu
-    pkgs.fira-code
-    pkgs.nerdfonts
-
-    # security
-    pkgs.gnupg
-    pkgs.pass
-    pkgs.pinentry-gtk2
-
-    # applications
-    pkgs.browserpass
-    pkgs.es
-    pkgs.firefox
-    pkgs.gimp
-    #pkgs.shotgun # TODO nixpkgs: shotgun does not support aarch64-linux.
-    #pkgs.slack # TODO nixpkgs: slack does not support aarch64-linux.
-    pkgs.mutt
-    pkgs.taskwarrior
-    pkgs.timewarrior
-    pkgs.tmux
-    pkgs.vim
-    pkgs.vimpager-latest
-    pkgs.w3m
-    pkgs.weechat
-    pkgs.wezterm
-    pkgs.zathura
-
-    # command line tools
-    pkgs.awscli
-    pkgs.bottom
-    pkgs.tree
-    pkgs.curl
-    pkgs.direnv
-    pkgs.du-dust
-    pkgs.exa
-    pkgs.nix-index
-    pkgs.fd
-    pkgs.feh
-    pkgs.gh
-    pkgs.git
-    pkgs.glab
-    pkgs.glow
-    pkgs.httpie
-    pkgs.inotify-tools
-    pkgs.jq
-    pkgs.macchina
-    pkgs.nailgun
-    pkgs.onefetch
-    pkgs.pandoc
-    #pkgs.polipo # TODO nixpkgs: insecure package.
-    pkgs.pre-commit
-    pkgs.procs
-    pkgs.readline
-    pkgs.ripgrep
-    pkgs.sd
-    pkgs.slop
-    pkgs.sqlite
-    #pkgs.ssm-session-manager-plugin # TODO nixpkgs: ssm-session-manager-plugin does not support aarch64-linux.
-    pkgs.tealdeer
-    pkgs.tokei
-    pkgs.unixtools.netstat
-    pkgs.xclip
-
-    # development
-    pkgs.python310
-  ];
-
 }
---
+++ home/packages.nix
@@ -0,0 +1,72 @@
+{ pkgs, ... }:
+
+{
+  home.packages = with pkgs; [
+    # desktop environment
+    dmenu
+    fira-code
+    i3
+    i3status-rust
+    nerdfonts
+
+    # security
+    gnupg
+    pass
+    pinentry-gtk2
+
+    # applications
+    bluez
+    browserpass
+    es
+    firefox
+    gimp
+    mutt
+    pavucontrol
+    shotgun
+    taskwarrior
+    timewarrior
+    tmux
+    vim
+    vimpager-latest
+    w3m
+    weechat
+    wezterm
+
+    # command line tools
+    awscli
+    bottom
+    curl
+    direnv
+    du-dust
+    exa
+    fd
+    feh
+    gh
+    git
+    glab
+    glow
+    httpie
+    inotify-tools
+    jq
+    macchina
+    nailgun
+    nix-index
+    onefetch
+    pandoc
+    pre-commit
+    procs
+    readline
+    ripgrep
+    sd
+    slop
+    sqlite
+    tealdeer
+    tokei
+    tree
+    unixtools.netstat
+    xclip
+
+    # development
+    python310
+  ];
+}
