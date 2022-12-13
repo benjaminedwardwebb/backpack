d8b4c7ef2c9cbc746e13fc24a2f50a86087fce22
Benjamin Edward Webb <benjaminedwardwebb@gmail.com>
2022-12-13 12:46:02 -0500

    taskwarrior: add taskserver synchronization
    Secrets are being stored on the local host at ~/.config/task/keys. As
    far as I can tell there's no way to store them other than in plaintext
    files, since taskwarrior expects a path to the private key and cert
    files in the taskrc. In general I'm not a fan of taskserver's
    authentication mechanisms.
    
    See: https://github.com/GothenburgBitFactory/taskwarrior/issues/1944
    See: https://github.com/GothenburgBitFactory/taskwarrior/issues/2115

--- home/taskwarrior/default.nix
+++ home/taskwarrior/default.nix
@@ -1,7 +1,8 @@
-{ config, pkgs, lib, ... }:
+{ pkgs, ... }:
 
 {
   programs.taskwarrior.enable = true;
   programs.taskwarrior.colorTheme = ./solarized-light-256;
+  programs.taskwarrior.extraConfig = builtins.readFile ./taskrc;
   xdg.configFile."task/hooks".source = ./hooks;
 }
